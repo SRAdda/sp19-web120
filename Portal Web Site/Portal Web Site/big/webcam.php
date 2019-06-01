44d9da87f5b4b883eec508e646ba8599eb0f187f
Sally <46608728+SRAdda@users.noreply.github.com>
2019-05-31 22:30:20 -0700

    Updates BIG website with added elements

---
+++ Portal Web Site/Portal Web Site/big/webcam.php
@@ -0,0 +1,18 @@
+<?php include "includes/header.php"?>
+
+<div class='embed-container'>
+  <iframe src='https://www.youtube.com/embed/4993sBLAzGA' allowfullscreen></iframe>
+</div> 
+</section>   
+<!-- END LEFT COLUMN -->
+        
+<!-- START RIGHT COLUMN -->
+<aside>
+<h3>DOT Camera</h3>
+  <div class="dot"> 
+    <iframe src="webcam.html"></iframe>
+  </div>
+</aside>
+<!-- END RIGHT COLUMN -->
+
+<?php include "includes/footer.php"?>
