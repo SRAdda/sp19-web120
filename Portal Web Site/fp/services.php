2a11b7b6711cde76138dfe64c82753bdafd21c8c
Sally <46608728+SRAdda@users.noreply.github.com>
2019-06-11 18:13:06 -0700

    updates final project code

---
+++ Portal Web Site/fp/services.php
@@ -0,0 +1,138 @@
+<?php include "includes/header.php"?>
+<link rel="stylesheet" href="css/tables.css"  />
+<table>
+    <thead>
+        <tr>
+            <th scope="col">Haircut & Style</th>
+            <th scope="col"></th>
+        </tr>
+    </thead>
+        <tr>
+            <td>Women</td>
+            <td><sm>from</sm> $100</td>
+        </tr>
+        <tr>
+<td>Men</td>
+<td><sm>from</sm> $60</td>
+</tr>
+        <tr>
+    <td>Children<br />
+        <em>11 years and younger</em></td>
+        <td><sm>from</sm> $30</td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <th>Color</th>
+            <th></th>
+        </tr>
+     <tr>
+<td>Women</td>
+<td><sm>from</sm> $80</td>
+</tr>
+<tr>
+<td>Men</td>
+<td><sm>from</sm> $45</td>
+</tr>
+<tr>
+<td>Full Foil</td>
+<td><sm>from</sm> $130</td>
+</tr>
+<tr>
+<td>Partial Foil</td>
+<td><sm>from</sm> $60</td>
+</tr>
+<tr>
+<td>Bleach &amp; tone</td>
+<td><sm>from</sm> $120</td>
+</tr>
+<tr>
+<td>Full Balayage</td>
+<td><sm>from</sm> $120</td>
+</tr>
+<tr>
+<td>Partial Balayage</td>
+<td><sm>from</sm> $60</td>
+</tr>
+<tr>
+    <th>Texturizer</th>
+    <th></th>
+</tr>
+    <tr>
+        <td>Perm</td>
+        <td><sm>from</sm> $90</td>
+    </tr>
+<tr>
+<td>Relaxer</td>
+<td><sm>from</sm> $110</td>
+</tr>
+<tr>
+<td></td>
+<td></td>
+</tr>
+<tr>
+<th>Treatments</th>
+<th></th>
+</tr>
+<tr>
+<td>Keratin</td>
+<td ><sm>from</sm> $150</td>
+</tr>
+<tr>
+<td>Intensive Conditioning</td>
+<td><sm>from</sm> $20</td>
+</tr>
+<tr>
+<td></td>
+<td></td>
+</tr>
+<tr>
+<th>Hair extensions </th>
+    <th></th>
+</tr>
+<tr>
+<td>Partial</td>
+<td><sm>from</sm> $60</td>
+</tr>
+<tr>
+<td>Full</td>
+<td>$200</td>
+</tr>
+<tr>
+<td></td>
+<td></td>
+</tr>
+<tr>
+<th>Special-occasion </th>
+<th></th>
+</tr>
+<tr>
+<td>Weddings</td>
+<td>$100 per hour<em><br />
+</em><em>(Free Initial Consult)</em></td>
+</tr>
+<tr>
+<td>Up-do</td>
+<td>$65</td>
+</tr>
+<tr>
+<td>Blow Dry</td>
+<td>$45</td>
+</tr>
+<tr>
+<td>Makeup</td>
+<td>$50</td>
+</tr>
+
+</table>
+
+ <aside class="address">
+
+      <p class="aside">Appointments&#58;<br> Salon QE<br>
+     2520 1st Avenue<br> Seattle, WA 98121<br>   
+            <a href="tel:012064410304">206.441.0304</a></p>
+  </aside> 
+  <!-- END address info COLUMN -->
+<?php include "includes/footer.php"?>
